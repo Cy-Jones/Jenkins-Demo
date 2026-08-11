39d4bab8056d883994637dd8ab778ae6ded8a81f
Cyrus Kpakolo Jones <cyruskjones@gmail.com>
2026-08-11 11:27:52 +0530

    Create App.java

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Cyrus! Your Jenkins Practical 5 Build works successfully.");
+    }
+}
